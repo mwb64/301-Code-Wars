50468ec23bada80597bf62017900695aaa24aed8
Your Name <you@example.com>
2018-01-02 18:04:36 -0800

    folder clean-up

--- nice-array/solutions.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-function test(el, index, arr){
-  return (arr.includes(el+1) || arr.includes(el-1));
-}
-function isNice(arr){
-return arr.every(test);
-}
--- replace-all-dots/solutions.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-var re = /[.]/g; 
-var replaceDots = function(str) {
-  return str.replace(re, '-');
-}
-replaceDots("one.two.three");
